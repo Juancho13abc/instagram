e9534e112057d0f9740adb7121c853658931e9fe
Juancho13abc <juan_munarca@fet.edu.co>
2026-05-13 12:01:59 -0500

    Agregando index.php en la raiz para redireccionar a public

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * This file allows us to run the application from the root directory.
+ */
+
+require_once __DIR__.'/public/index.php';
