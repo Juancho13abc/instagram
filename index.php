2fe94ba9ca4d9f8b91aa16581079f2ae708d3c6c
Juancho13abc <juan_munarca@fet.edu.co>
2026-05-15 08:15:58 -0500

    Estructura plana de Laravel para despliegue en Azure

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * This file allows us to run the application from the root directory.
- */
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
-require_once __DIR__.'/public/index.php';
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
