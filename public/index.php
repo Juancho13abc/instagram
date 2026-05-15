9d71f376b77a04c3f3fdb63dbc026a0494e49fca
Juancho13abc <juan_munarca@fet.edu.co>
2026-05-15 11:41:18 -0500

    Auto-crear carpetas de storage para Azure

--- public/index.php
+++ public/index.php
@@ -5,6 +5,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+foreach (['views', 'cache', 'cache/data', 'sessions'] as $dir) {
+    if (!is_dir(__DIR__.'/../storage/framework/'.$dir)) {
+        @mkdir(__DIR__.'/../storage/framework/'.$dir, 0777, true);
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
